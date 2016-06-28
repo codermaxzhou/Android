b313a90d581691d0fde9177cd296f95f74644516
Yihao Zhou <yihaozhou1991@gmail.com>
2016-06-28 16:48:06 -0400

    FlickrBrowser Update

---
+++ FlickrBrowser/app/src/main/java/com/example/yzhou2/flickrbrowser/BaseActivity.java
@@ -0,0 +1,23 @@
+package com.example.yzhou2.flickrbrowser;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by yzhou2 on 6/28/2016.
+ */
+public class BaseActivity extends AppCompatActivity{
+
+    private Toolbar mToolbar;
+
+    protected Toolbar activateToolbar() {
+        if (mToolbar == null) {
+            mToolbar = (Toolbar) findViewById(R.id.app_bar);
+            if (mToolbar != null) {
+                setSupportActionBar(mToolbar);
+            }
+        }
+        return mToolbar;
+    }
+}
--- FlickrBrowser/app/src/main/java/com/example/yzhou2/flickrbrowser/MainActivity.java
+++ FlickrBrowser/app/src/main/java/com/example/yzhou2/flickrbrowser/MainActivity.java
@@ -2,15 +2,15 @@ package com.example.yzhou2.flickrbrowser;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     private static final String LOG_TAG = "MainActivity";
     private List<Photo> mPhotosList = new ArrayList<Photo>();
@@ -21,13 +21,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-
-//        GetRawData theRawData = new GetRawData("https://api.flickr.com/services/feeds/photos_public.gne?tags=android,lollipop&format=json&nojsoncallback=1");
-        GetFlickrJsonData jsonData = new GetFlickrJsonData("android, lollipop", true);
-        jsonData.execute();
+        ProcessPhotos processPhotos = new ProcessPhotos("google", true);
+        processPhotos.execute();
     }
 
     @Override
